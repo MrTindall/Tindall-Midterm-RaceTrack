0749b02329c2235fadce605391c5fd6d9c6f7bef
Alex <tindall.al@gmail.com>
2023-10-12 17:03:26 -0500

    Add project files.

---
+++ RaceTrack/Cars/GTR.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    internal class GTR : RaceCar
+    {
+        public GTR() {
+            Name = "GTR";
+            TopSpeed = 195;
+        }
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} engine Roars up");
+        }
+
+        public override void Brake()
+        {
+            base.Brake();
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine($"The {Name} engine turns off");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/VinDiesel.cs
@@ -0,0 +1,23 @@
+﻿using RaceTrack.RaceTrack.Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    internal class VinDiesel : Driver
+    {
+        public VinDiesel(RaceCar car) : base(car)
+        {
+            Name = "Vin Diesel";
+            SkillLevel = 10;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
